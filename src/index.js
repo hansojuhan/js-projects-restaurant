b75cfe80673ac7ea5d016b1add0a176121ac34fd
hje <hjerikson@gmail.com>
2024-10-09 12:39:22 +0200

    Navbar loading + styles

--- src/index.js
+++ src/index.js
@@ -1,11 +1,51 @@
 import image1 from "./img/resto.webp"; 
 import './styles.css';
 
+function loadNavbar() {
+  const header = document.createElement('header');
+
+  const nav = document.createElement('nav');
+  
+  const title = document.createElement('h1');
+  title.innerText = 'ZEN SUSHI';
+  title.id = 'title';
+  nav.append(title);
+
+  const menuList = document.createElement('ul');
+
+  // Home link
+  let homeMenuLink = document.createElement('li');
+  let homeMenulinkButton = document.createElement('a');
+  homeMenulinkButton.innerText = 'Home';
+  homeMenulinkButton.id = 'menu-link';
+  homeMenuLink.append(homeMenulinkButton);
+  menuList.append(homeMenuLink);
+
+  // Menu link
+  let menuMenuLink = document.createElement('li');
+  let menuMenulinkButton = document.createElement('a');
+  menuMenulinkButton.innerText = 'Menu';
+  menuMenulinkButton.id = 'menu-link';
+  menuMenuLink.append(menuMenulinkButton);
+  menuList.append(menuMenuLink);
+
+  // About link
+  let aboutMenuLink = document.createElement('li');
+  let aboutMenulinkButton = document.createElement('a');
+  aboutMenulinkButton.innerText = 'About';
+  aboutMenulinkButton.id = 'menu-link';
+  aboutMenuLink.append(aboutMenulinkButton);
+  menuList.append(aboutMenuLink);
+
+  nav.append(menuList);
+  header.append(nav);
+
+  // Insert into body, header, before content
+  body.insertBefore(header, content);
+}
+
 // Generates all the markup inside div#content
 function loadContent() {
-  // Find the parent div#content
-  const content = document.getElementById('content');
-
   // Container div for first section
   const firstSection = document.createElement('div');
   firstSection.classList.add('section-container');
@@ -24,7 +64,7 @@ function loadContent() {
   textContent.id = 'main-text';
 
   const h1 = document.createElement('h1');
-  h1.innerText = 'SUSHI ZEN';
+  h1.innerText = 'ZEN SUSHI';
   textContent.append(h1);
 
   const a = document.createElement('a');
@@ -46,4 +86,10 @@ function loadContent() {
   content.append(firstSection);
 }
 
+// Find the main body
+const body = document.querySelector('body');
+// Find the parent div#content
+const content = document.getElementById('content');
+
+loadNavbar();
 loadContent();
