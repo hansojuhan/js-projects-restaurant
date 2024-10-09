66c1bc8170a79167e125d4c831561cdd20a7e00d
hje <hjerikson@gmail.com>
2024-10-09 17:35:11 +0200

    Finish content loading + styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import image1 from "./img/resto.webp"; 
+import image2 from "./img/resto2.webp"; 
+import image3 from "./img/resto3.webp"; 
 import './styles.css';
 
 function loadNavbar() {
@@ -45,7 +47,7 @@ function loadNavbar() {
 }
 
 // Generates all the markup inside div#content
-function loadContent() {
+function loadFirstSection() {
   // Container div for first section
   const firstSection = document.createElement('div');
   firstSection.classList.add('section-container');
@@ -86,6 +88,60 @@ function loadContent() {
   content.append(firstSection);
 }
 
+// Generates all the markup inside div#content
+function loadSecondSection() {
+  // Container div for first section
+  const secondSection = document.createElement('div');
+  secondSection.classList.add('section-container');
+
+  // Create text to the right in a div
+  const textContent = document.createElement('div');
+  textContent.id = 'main-description';
+
+  const h2 = document.createElement('h2');
+  h2.innerText = 'ZEN SUSHI Journey';
+  textContent.append(h2);
+
+  const p = document.createElement('p');
+  p.innerText = 'Discover New Flavors, New All You Can Eat Experience'
+  textContent.append(p);
+
+  const p2 = document.createElement('p');
+  p2.innerText = 'Sushi Zen is a sensory voyage, where traditional Japanese cuisine meets the bold vibrancy of South American flavors. A completely redefined Sushi Experience, where originality, exclusive atmosphere, and attention to detail become the hallmark of distinction.'
+  textContent.append(p2);
+  
+  const p3 = document.createElement('p');
+  p3.innerText = 'The ambiance is refined, elegant, and sophisticated, yet warm and intimate. It’s designed to recreate the tranquility and serenity of a Japanese garden, with soft-toned vertical gardens offering a calming retreat.'
+  textContent.append(p3);
+  
+  const p4 = document.createElement('p');
+  p4.innerText = "The All You Can Eat menu is crafted to amaze: alongside classic sashimi, rolls, and nigiri, you'll find delightful excursions into South American cuisine with tacos, tropical tartare, signature rolls, tempura, and other unique dishes."
+  textContent.append(p4);   
+
+  const p5 = document.createElement('p');
+  p5.innerText = "Sushi Zen offers a true culinary experience, ensuring an unlimited explosion of innovative and captivating flavors."
+  textContent.append(p5);  
+
+  secondSection.append(textContent);
+
+  // Image part
+  const imageDiv = document.createElement('div');
+  imageDiv.id = 'secondary-image';
+
+  const imageContent1 = document.createElement('img');
+  imageContent1.src = image2;
+  imageDiv.append(imageContent1);
+
+  const imageContent2 = document.createElement('img');
+  imageContent2.src = image3;
+  imageDiv.append(imageContent2);
+
+  secondSection.append(imageDiv);
+
+  // Append section div to content div
+  content.append(secondSection);
+}
+
 function loadFooter() {
   const footer = document.createElement('footer');
 
@@ -112,5 +168,6 @@ const body = document.querySelector('body');
 const content = document.getElementById('content');
 
 loadNavbar();
-loadContent();
+loadFirstSection();
+loadSecondSection();
 loadFooter();
